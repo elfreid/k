aaf836cff3b1b8b76c6abbbc7af56b4cf758ce34
Ryan Andri <ryanandri@linuxmail.org>
2018-04-18 23:55:09 +0700

    drivers: power: fixed offline charging leds
    * Adapted from Xiaomi markw (Redmi4 Prime) changes.
    * changes to apply only on init charger mode because if we merging
      this all changes from Xiaomi it will applied to boot system that
      make leds charging can't be set to other colors, so apply this
      changes only on init charger mode.
    
    * [elfreid] Adapt to ulysse which only have white led, but use "red"
      as led class name
    
    Signed-off-by: elfreid <elfreid@protonmail.com>

--- include/linux/power_supply.h
+++ include/linux/power_supply.h
@@ -464,6 +464,10 @@ struct power_supply {
 	char *online_trig_name;
 	struct led_trigger *charging_blink_full_solid_trig;
 	char *charging_blink_full_solid_trig_name;
+
+	/* Offline charging leds */
+	struct led_trigger *charging_red_trig;
+	char *charging_red_trig_name;
 #endif
 };
 
